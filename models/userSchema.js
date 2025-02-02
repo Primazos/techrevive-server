f6393b316e992314f23740903d65e6320fa86ac3
Jorge-Guedes <jorgeguedespena@alumno.ieselrincon.es>
2025-02-02 17:13:57 +0000

    deleted comment

--- models/userSchema.js
+++ models/userSchema.js
@@ -21,27 +21,3 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("User", userSchema);
 
 export default User;
-
-/* import mongoose from "mongoose";
-
-const locationSchema = new mongoose.Schema({
-  city: { type: String },
-  region: { type: String },
-  country: { type: String },
-});
-
-const userSchema = new mongoose.Schema({
-  _id: { type: String, required: true },
-  name: { type: String, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
-  avatar_img: { type: String },
-  gender: { type: String },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  location: { type: locationSchema },
-});
-
-const User = mongoose.model("User", userSchema);
-
-export default User; */
